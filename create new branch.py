265a38e98458c52bb489295c7310f4a95b476613
Anthony Guijarro <Anthonyg5005@users.noreply.huggingface.co>
2024-01-25 16:33:54 +0000

    Update create new branch.py

--- create new branch.py
+++ create new branch.py
@@ -4,10 +4,11 @@ from huggingface_hub import create_branch
 
 #get user variables
 repo = input("Repository name: ")
+r_type = input("Repo type (model) (dataset) (space): ")
 branch = input("New branch name: ")
 
 #login
 huggingface_hub.login(os.environ['HF_TOKEN'])
 
 #create the branch
-create_branch(f"{repo}", repo_type="model", branch=f"{branch}")
+create_branch(repo, repo_type=r_type, branch=branch)
