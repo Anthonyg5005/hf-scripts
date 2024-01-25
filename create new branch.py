a8d61eeff7449979d3128b32a6f7402adbed1dde
Anthony Guijarro <Anthonyg5005@users.noreply.huggingface.co>
2024-01-25 20:59:05 +0000

    ensure user chose valid option

--- create new branch.py
+++ create new branch.py
@@ -5,14 +5,21 @@ from huggingface_hub import create_branch
 
 #get user variables
 repo = input("Repository name: ")
-r_type = input("Repo type (model) (dataset) (space): ")
+while True:
+    r_type = input("Repo type (model) (dataset) (space): ").lower()
+    
+    if r_type not in ['model', 'dataset', 'space']:
+        print("Please choose one of the three options.")
+        continue
+    
+    break
 branch = input("New branch name (No spaces): ")
 
 #get token
 if 'HF_TOKEN' in os.environ:
     #if the variable is found then write it to hf_token:
     hf_token = os.environ['HF_TOKEN']
-    tfound = "where are my doritos?"
+    tfound = "true"
 else:
     #if the variable is not found then prompt user to provide it:
     hf_token = input("HF_TOKEN Variable not detected. Enter your HuggingFace (WRITE) token: ")
@@ -25,15 +32,14 @@ huggingface_hub.login(hf_token)
 create_branch(repo, repo_type=r_type, branch=branch)
 
 #extra information
-
-#Won't work if special characters are used
+#won't work if special characters are used
 if r_type == model:
     print(f"Branch created at https://huggingface/{repo}/tree/{branch}")
 elif r_type == dataset:
     print(f"Branch created at https://huggingface/datasets/{repo}/tree/{branch}")
 elif r_type == space:
     print(f"Branch created at https://huggingface/spaces/{repo}/tree/{branch}")
-
+#if token wasn't found then display following text:
 if tfound == 'false':
     print('''
           Set HF_TOKEN to a token with WRITE permissions to skip inputting token on each run.
@@ -47,3 +53,4 @@ if tfound == 'false':
           (System Restart Required)
           To temporarily set a token to the active terminal use 'set HF_TOKEN=Your-HuggingFace-token-here'
           ''')
+input("Press enter to continue.")
