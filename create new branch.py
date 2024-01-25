d5e11700c556369f18d587b43c25086e2fc64f8c
Anthony Guijarro <Anthonyg5005@users.noreply.huggingface.co>
2024-01-25 19:53:49 +0000

    add login token detection and helpful notes

--- create new branch.py
+++ create new branch.py
@@ -1,3 +1,4 @@
+#import required modules
 import os
 import huggingface_hub
 from huggingface_hub import create_branch
@@ -7,8 +8,16 @@ repo = input("Repository name: ")
 r_type = input("Repo type (model) (dataset) (space): ")
 branch = input("New branch name: ")
 
+#get token
+if 'HF_TOKEN' in os.environ:
+    #if the variable is found then write it to hf_token:
+    hf_token = os.environ['HF_TOKEN']
+else:
+    #if the variable is not found then prompt user to provide it:
+    hf_token = input("HF_TOKEN Variable not detected. Enter your HuggingFace token: ")
+
 #login
-huggingface_hub.login(os.environ['HF_TOKEN'])
+huggingface_hub.login(hf_token)
 
 #create the branch
 create_branch(repo, repo_type=r_type, branch=branch)
