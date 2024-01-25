a4ebec918f68250f28c835e368faf2c7f419b6b6
Anthony Guijarro <Anthonyg5005@users.noreply.huggingface.co>
2024-01-25 20:41:33 +0000

    added useful notes
    testing needed

--- create new branch.py
+++ create new branch.py
@@ -6,18 +6,44 @@ from huggingface_hub import create_branch
 #get user variables
 repo = input("Repository name: ")
 r_type = input("Repo type (model) (dataset) (space): ")
-branch = input("New branch name: ")
+branch = input("New branch name (No spaces): ")
 
 #get token
 if 'HF_TOKEN' in os.environ:
     #if the variable is found then write it to hf_token:
     hf_token = os.environ['HF_TOKEN']
+    tfound = "where are my doritos?"
 else:
     #if the variable is not found then prompt user to provide it:
-    hf_token = input("HF_TOKEN Variable not detected. Enter your HuggingFace token: ")
+    hf_token = input("HF_TOKEN Variable not detected. Enter your HuggingFace (WRITE) token: ")
+    tfound = "false"
 
 #login
 huggingface_hub.login(hf_token)
 
 #create the branch
 create_branch(repo, repo_type=r_type, branch=branch)
+
+#extra information
+
+#Won't work if special characters are used
+if r_type == model:
+    print(f"Branch created at https://huggingface/{repo}/tree/{branch}")
+elif r_type == dataset:
+    print(f"Branch created at https://huggingface/datasets/{repo}/tree/{branch}")
+elif r_type == space:
+    print(f"Branch created at https://huggingface/spaces/{repo}/tree/{branch}")
+
+if tfound == 'false':
+    print('''
+          Set HF_TOKEN to a token with WRITE permissions to skip inputting token on each run.
+          
+          On Unix systems, edit the file ~/.bashrc with an editor of your choise.
+          On a new line add: export HF_TOKEN=Your-HuggingFace-token-here
+          (Terminal Refresh Required)
+          To temporarily set a token to the active terminal use 'export HF_TOKEN=Your-HuggingFace-token-here'
+          
+          On Windows use 'setx HF_TOKEN Your-HuggingFace-token-here'
+          (System Restart Required)
+          To temporarily set a token to the active terminal use 'set HF_TOKEN=Your-HuggingFace-token-here'
+          ''')
