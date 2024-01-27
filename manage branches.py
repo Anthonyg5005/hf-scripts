f0e50258d43c1073bfd8eed89a4710d1c44b4e3a
Anthony Guijarro <Anthonyg5005@users.noreply.huggingface.co>
2024-01-27 03:44:46 +0000

    update to new token finding method and verify valid WRITE token

--- manage branches.py
+++ manage branches.py
@@ -1,7 +1,6 @@
 #import required modules
 import os
-import huggingface_hub
-from huggingface_hub import create_branch, delete_branch
+from huggingface_hub import create_branch, delete_branch, login, get_token, whoami
 
 #set clear screen function
 def clear_screen():
@@ -16,9 +15,8 @@ while True:
     
     if cord not in ['create', 'delete']:
         clear_screen()
-        print("Please choose one of the two options.")
+        print("Please choose one of the following two options.")
         continue
-    
     break
 clear_screen()
 repo = input("Repository name (User/Repo): ")
@@ -28,26 +26,38 @@ while True:
     
     if r_type not in ['model', 'dataset', 'space']:
         clear_screen()
-        print("Please choose one of the three options.")
+        print("Please choose one of the following three options.")
         continue
-    
     break
 clear_screen()
 branch = input("Branch name (No spaces): ")
 clear_screen()
 
 #get token
-if 'HF_TOKEN' in os.environ:
-    #if the variable is found then write it to hf_token:
-    hf_token = os.environ['HF_TOKEN']
-    tfound = "Where are my doritos?"
-else:
-    #if the variable is not found then prompt user to provide it:
-    hf_token = input("HF_TOKEN Variable not detected. Enter your HuggingFace (WRITE) token: ")
+if 'None' in str(get_token()):
+    #if the token is not found then prompt user to provide it:
+    hf_token = input("API token not detected. Enter your HuggingFace (WRITE) token: ")
     tfound = "false"
+else:
+    #if the token is found then write it to hf_token:
+    hf_token = get_token()
+    tfound = "Where are my doritos?"
 
 #login
-huggingface_hub.login(hf_token)
+login(hf_token)
+
+#if the token is read only then prompt user to provide a write token:
+while True:
+    if whoami().get('auth', {}).get('accessToken', {}).get('role', None) != 'write':
+        clear_screen()
+        print("You do not have write access to this repository. Please use a valid token with (WRITE) access.")
+        hf_token = input("Enter your HuggingFace (WRITE) token: ")
+        login(hf_token)
+        continue
+    break
+
+#store the user's name
+fname = whoami().get('fullname', None)
 
 #create or delete the branch
 if cord == 'create':
@@ -60,30 +70,24 @@ clear_screen()
 #won't work if special characters are used but should still successfully be created/deleted
 if cord == 'create':
     if r_type == 'model':
-        print(f"Branch created at https://huggingface.co/{repo}/tree/{branch}")
+        print(f"Branch {branch} created at https://huggingface.co/{repo}/tree/{branch}")
     elif r_type == 'dataset':
-        print(f"Branch created at https://huggingface.co/datasets/{repo}/tree/{branch}")
+        print(f"Branch {branch} created at https://huggingface.co/datasets/{repo}/tree/{branch}")
     elif r_type == 'space':
-        print(f"Branch created at https://huggingface.co/spaces/{repo}/tree/{branch}")
+        print(f"Branch {branch} created at https://huggingface.co/spaces/{repo}/tree/{branch}")
 else:
     if r_type == 'model':
-        print(f"Branch deleted on {r_type} https://huggingface.co/{repo}")
+        print(f"Branch {branch} deleted on {r_type} https://huggingface.co/{repo}")
     elif r_type == 'dataset':
-        print(f"Branch deleted on {r_type} https://huggingface.co/datasets/{repo}")
+        print(f"Branch {branch} deleted on {r_type} https://huggingface.co/datasets/{repo}")
     elif r_type == 'space':
-        print(f"Branch deleted on {r_type} https://huggingface.co/spaces/{repo}")
+        print(f"Branch {branch} deleted on {r_type} https://huggingface.co/spaces/{repo}")
 #if token wasn't found then display following text:
 if tfound == 'false':
-    print('''
-          Set HF_TOKEN to a token with WRITE permissions to skip inputting token on each run.
+    print(f'''
+          You are now logged in as {fname}.
           
-          On Unix systems, edit the file ~/.bashrc with an editor of your choise.
-          On a new line add: export HF_TOKEN=Your-HuggingFace-token-here
-          (Terminal Refresh Required)
-          To temporarily set a token to the active terminal use 'export HF_TOKEN=Your-HuggingFace-token-here'
-          
-          On Windows use 'setx HF_TOKEN Your-HuggingFace-token-here'
-          (System Restart Required)
-          To temporarily set a token to the active terminal use 'set HF_TOKEN=Your-HuggingFace-token-here'
+          To logout, use the cli 'huggingface-cli logout'
+          To view your active account, use 'huggingface-cli whoami'
           ''')
 input("Press enter to continue.")
