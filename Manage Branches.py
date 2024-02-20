778074cddf2d83a991c2b7c7d2b7b6b0939713b9
Anthony Guijarro <Anthonyg5005@users.noreply.huggingface.co>
2024-02-20 21:54:22 +0000

    Fix loop when having a read only HF_TOKEN variable

--- Manage Branches.py
+++ Manage Branches.py
@@ -76,20 +76,25 @@ else:
     tfound = "false"
 
 #if the token is read only then prompt user to provide a write token:
-envariable = 'false for now'
 while True:
     if whoami().get('auth', {}).get('accessToken', {}).get('role', None) != 'write':
+        clear_screen()
+        if os.environ.get('HF_TOKEN', None) is not None: #if environ finds HF_TOKEN as write then display following text and exit:
+            print(f'''
+          You have the environment variable HF_TOKEN set.
+          You cannot log in.
+          Either set the environment variable to a (WRITE) token or remove it.
+          ''')
+            exit()
+            
         if os.environ.get('KAGGLE_KERNEL_RUN_TYPE', None) is not None: #check if user in kaggle
             print('''
                   When using Kaggle, make sure to use the secret key HF_TOKEN with a 'WRITE' token.
                   This will prevent the need to login every time you run the script.
                   Set your secrets with the secrets add-on on the top of the screen.
                   ''')
-        clear_screen()
         print("You do not have write access to this repository. Please use a valid token with (WRITE) access.")
         login(input("Enter your HuggingFace (WRITE) token: "))
-        if os.environ.get('HF_TOKEN', None) is not None:
-            envariable = 'true'
         continue
     break
 
@@ -163,10 +168,4 @@ if tfound == 'false':
           To view your active account, use 'huggingface-cli whoami'
           ''')
 
-#if environ finds HF_TOKEN then display following text:
-if envariable == 'true':
-    print(f'''
-          You have the environment variable HF_TOKEN set.
-          This may interfere with the huggingface login every time script is ran.
-          ''')
 input("Press enter to continue.")
