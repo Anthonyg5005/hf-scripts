c9fc91d56fa1d429628c4cbe3070e5622f77b7ad
Anthony Guijarro <Anthonyg5005@users.noreply.huggingface.co>
2024-02-22 14:55:25 +0000

    Update HF Login Snippet Kaggle.py

--- HF Login Snippet Kaggle.py
+++ HF Login Snippet Kaggle.py
@@ -3,20 +3,23 @@ from huggingface_hub import login, get_token, whoami
 import os
 
 #get token
-if get_token() is not None:
-    #if the token is found then log in:
-    login(get_token())
-    tfound = "Where are my doritos?" #remove if lines 55-61 removed
-else:
-    if os.environ.get('KAGGLE_KERNEL_RUN_TYPE', None) is not None: #check if user in kaggle
+if os.environ.get('KAGGLE_KERNEL_RUN_TYPE', None) is not None: #check if user in kaggle
+    from kaggle_secrets import UserSecretsClient
+    if UserSecretsClient().get_secret("HF_TOKEN") is not None:
+        login(UserSecretsClient().get_secret("HF_TOKEN"))
+    else:
         print('''
-              When using Kaggle, make sure to use the secret key HF_TOKEN with a 'WRITE' token.
-              This will prevent the need to login every time you run the script.
-              Set your secrets with the secrets add-on on the top of the screen.
-              ''')
+            When using Kaggle, make sure to use the secret key HF_TOKEN with a 'WRITE' token.
+            This will prevent the need to login every time you run the script.
+            Set your secrets with the secrets add-on on the top of the screen.
+             ''')
+if get_token() is not None:
+    if os.environ.get('KAGGLE_KERNEL_RUN_TYPE', None) is None:
+        #if user not in kaggle and the token is found then log in:
+        login(get_token())
+else:
     #if the token is not found then prompt user to provide it:
     login(input("API token not detected. Enter your HuggingFace (WRITE) token: "))
-    tfound = "false" #remove if lines 55-61 removed
 
 #if the token is read only then prompt user to provide a write token (Only required if user needs a WRITE token, remove if READ is enough):
 while True:
@@ -27,25 +30,10 @@ while True:
           You cannot log in.
           Either set the environment variable to a (WRITE) token or remove it.
           ''')
-        if os.environ.get('KAGGLE_KERNEL_RUN_TYPE', None) is not None: #check if user in kaggle
-            print('''
-                  When using Kaggle, make sure to use the secret key HF_TOKEN with a 'WRITE' token.
-                  This will prevent the need to login every time you run the script.
-                  Set your secrets with the secrets add-on on the top of the screen.
-                  ''')
             input("Press enter to continue.")
             exit()
         print("You do not have write access to this repository. Please use a valid token with (WRITE) access.")
         login(input("Enter your HuggingFace (WRITE) token: "))
         continue
     break
-
-#if token wasn't found at first then print the name of the new logged in user (OPTIONAL)
-if tfound == 'false':
-    print(f'''
-          You are now logged in as {whoami().get('fullname', None)}.
-          
-          To logout, use the hf command line interface 'huggingface-cli logout'
-          To view your active account, use 'huggingface-cli whoami'
-          ''')
     
