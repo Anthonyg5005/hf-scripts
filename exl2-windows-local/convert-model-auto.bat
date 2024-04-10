99db3784d96d8c3ee0fc293664b9aad2cacc7c4f
Anthony Guijarro <anthonyxboxreal@gmail.com>
2024-04-10 16:05:48 -0500

    Remove WD folder when done
    and added info to instructions

--- exl2-windows-local/convert-model-auto.bat
+++ exl2-windows-local/convert-model-auto.bat
@@ -6,3 +6,4 @@ mkdir %model%-exl2-%bpw%bpw
 mkdir %model%-exl2-%bpw%bpw-WD
 copy %model%\config.json %model%-exl2-%bpw%bpw-WD
 venv\scripts\python.exe convert.py -i %model% -o %model%-exl2-%bpw%bpw-WD -cf %model%-exl2-%bpw%bpw -b %bpw%
+rmdir /s /q %model%-exl2-%bpw%-WD
