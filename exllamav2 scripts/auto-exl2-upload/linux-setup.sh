ee99e8cbbc2f8532507e56dc8efce9772353d005
Anthony Guijarro <anthonyxboxreal@gmail.com>
2025-02-12 15:14:21 -0600

    upgrade to new default pytorch version and instructions

--- exllamav2 scripts/auto-exl2-upload/linux-setup.sh
+++ exllamav2 scripts/auto-exl2-upload/linux-setup.sh
@@ -39,20 +39,11 @@ fi
 # if CUDA version 12 install pytorch for 12.1, else if CUDA 11 install pytorch for 11.8. If ROCm, install pytorch for ROCm 5.7
 read -p "Please enter your GPU compute version, CUDA 11/12 or AMD ROCm (11, 12, rocm): " pytorch_version
 
-# ask to install flash attention
-echo "Flash attention is a feature that could fix overflow issues on some more broken models, however, it will increase install time by a few hours."
-read -p "Would you like to install flash-attention? (rarely needed and optional) (y/n) " flash_attention
-if [ "$flash_attention" != "y" ] && [ "$flash_attention" != "n" ]; then
-    echo "Invalid input. Please enter y or n."
-    read -p "Press enter to continue"
-    exit
-fi
-
 if [ "$pytorch_version" = "11" ]; then
     echo "Installing PyTorch for CUDA 11.8"
     venv/bin/python -m pip install torch --index-url https://download.pytorch.org/whl/cu118 --upgrade
 elif [ "$pytorch_version" = "12" ]; then
-    echo "Installing PyTorch for CUDA 12.1"
+    echo "Installing PyTorch for CUDA 12.4"
     venv/bin/python -m pip install torch
 elif [ "$pytorch_version" = "rocm" ]; then
     echo "Installing PyTorch for AMD ROCm 5.7"
@@ -99,15 +90,6 @@ chmod +x start-quant.sh
 echo "#!/bin/bash" > enter-venv.sh
 echo "bash --init-file venv/bin/activate" >> enter-venv.sh
 chmod +x enter-venv.sh
-
-if [ "$flash_attention" = "y" ]; then
-    echo "Going to attempt to install flash attention but it isn't required."
-    echo "You may close now if you'd like and continue without flash attention."
-    read -p "Press enter to continue and install flash attention"
-    echo "Get some popcorn and watch a movie, this will take a while."
-    echo "Installing flash-attn..."
-    venv/bin/python -m pip install git+https://github.com/Dao-AILab/flash-attention.git
-fi
 
 echo "If you use ctrl+c to stop, you may need to also use 'pkill python' to stop running scripts."
 echo "Environment setup complete. run start-quant.sh to start the quantization process."
--- exllamav2 scripts/exl2-multi-quant-local/windows-setup.bat
+++ exllamav2 scripts/exl2-multi-quant-local/windows-setup.bat
@@ -40,23 +40,16 @@ if not "%exllamav2_version%"=="stable" if not "%exllamav2_version%"=="dev" (
 REM if CUDA version 12 install pytorch for 12.1, else if CUDA 11 install pytorch for 11.8
 echo CUDA compilers:
 where nvcc
+echo CUDA_HOME:
+echo %CUDA_HOME%
 set /p cuda_version="Please enter your CUDA version (11 or 12): "
-
-REM ask to install flash attention
-echo Flash attention is a feature that could fix overflow issues on some more broken models, however, it will increase install time by a few hours.
-set /p flash_attention="Would you like to install flash-attention? (rarely needed and optional) (y/n) "
-if not "%flash_attention%"=="y" if not "%flash_attention%"=="n" (
-    echo Invalid input. Please enter y or n.
-    pause
-    exit
-)
 
 if "%cuda_version%"=="11" (
     echo Installing PyTorch for CUDA 11.8...
     venv\scripts\python.exe -m pip install torch --index-url https://download.pytorch.org/whl/cu118 --upgrade
 ) else if "%cuda_version%"=="12" (
-    echo Installing PyTorch for CUDA 12.1...
-    venv\scripts\python.exe -m pip install torch --index-url https://download.pytorch.org/whl/cu121 --upgrade
+    echo Installing PyTorch for CUDA 12.4...
+    venv\scripts\python.exe -m pip install torch --index-url https://download.pytorch.org/whl/cu124 --upgrade
 ) else (
     echo Invalid CUDA version. Please enter 11 or 12.
     pause
@@ -98,15 +91,6 @@ echo pause >> start-quant.bat
 REM create enter-venv.bat
 echo @echo off > enter-venv.bat
 echo cmd /k call venv\scripts\activate.bat >> enter-venv.bat
-
-if "%flash_attention%"=="y" (
-    echo Going to attempt to install flash attention but it isn't required.
-    echo You may close now if you'd like and continue without flash attention.
-    pause
-    echo Get some popcorn and watch a movie. This will take a while.
-    echo Installing flash-attn...
-    venv\scripts\python.exe -m pip install git+https://github.com/Dao-AILab/flash-attention.git
-)
 
 powershell -c (New-Object Media.SoundPlayer "C:\Windows\Media\tada.wav").PlaySync();
 echo Environment setup complete. run start-quant.bat to start the quantization process.
